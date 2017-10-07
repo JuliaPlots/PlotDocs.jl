97e9cbac823c8bc2feef90db86bc170588b56d5a
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2017-10-07 05:56:21 -0700

    setup documenter

---
+++ docs/make.jl
@@ -0,0 +1,11 @@
+using Documenter, Plots
+
+makedocs(modules=[Plots], doctest=false)
+
+deploydocs(
+    deps   = Deps.pip("mkdocs",
+    "mkdocs-material" ,"python-markdown-math", "pygments", "pymdown-extensions"),
+    repo   = "github.com/JuliaPlots/PlotDocs.jl.git",
+    julia  = "0.6",
+    osname = "linux"
+)
