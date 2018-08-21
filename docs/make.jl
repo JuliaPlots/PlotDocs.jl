5c4fd735dd04a8dbff0c39d8210de1c66d38eede
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2018-08-21 12:16:31 +0200

    Specify versions for mkdocs
    Suggested in https://discourse.julialang.org/t/mkdocs-material-in-documenter/13764/4

--- docs/make.jl
+++ docs/make.jl
@@ -3,8 +3,8 @@ using Documenter, Plots
 makedocs(modules=[Plots], doctest=false)
 
 deploydocs(
-    deps   = Deps.pip("mkdocs",
-    "mkdocs-material" ,"python-markdown-math", "pygments", "pymdown-extensions"),
+    deps   = deps = Deps.pip("mkdocs==0.17.5", 
+        "mkdocs-material==2.9.4" ,"python-markdown-math", "pygments", "pymdown-extensions"),
     repo   = "github.com/JuliaPlots/PlotDocs.jl.git",
     julia  = "0.7",
     osname = "linux"
