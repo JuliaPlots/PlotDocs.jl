678277e28d477abf0a654f8b56156d9372bee02c
Daniel Schwabeneder <daschw@disroot.org>
2019-08-28 00:57:02 +0200

    add missing comma

--- docs/make.jl
+++ docs/make.jl
@@ -62,6 +62,6 @@ deploydocs(
     deps = Deps.pip("mkdocs==0.17.5",
         "mkdocs-material==2.9.4" ,"python-markdown-math", "pygments", "pymdown-extensions"),
     repo   = "github.com/JuliaPlots/PlotDocs.jl.git",
-    make   = () -> run(`mkdocs build`)
+    make   = () -> run(`mkdocs build`),
     target = "site"
 )
