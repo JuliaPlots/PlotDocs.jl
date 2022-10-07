e1cf5547e9bcdca2ad3bf452ca5350edb71fcc50
t-bltg <tf.bltg@gmail.com>
2022-10-07 21:20:00 +0200

    do not attemp to deploy if `DOCUMENTER_KEY` is unset

--- docs/make.jl
+++ docs/make.jl
@@ -172,9 +172,13 @@ foreach(galleries_cb) do cb
     cb()  # URL redirection for DemoCards-generated gallery
 end
 
-deploydocs(
-    repo = "github.com/JuliaPlots/PlotDocs.jl.git",
-    push_preview = true,
-    versions = ["stable" => "v^", "v#.#", "dev" => "dev", "latest" => "dev"],
-    forcepush = true,
-)
+if haskey(ENV, "DOCUMENTER_KEY")
+    deploydocs(
+        repo = "github.com/JuliaPlots/PlotDocs.jl.git",
+        push_preview = true,
+        versions = ["stable" => "v^", "v#.#", "dev" => "dev", "latest" => "dev"],
+        forcepush = true,
+    )
+else
+    @info "`DOCUMENTER_KEY` is empty, skip `deploydocs`"
+end
