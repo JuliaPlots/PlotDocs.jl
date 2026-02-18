31b423627243e7165cb0ff66334696fac9d6de00
t-bltg <tf.bltg@gmail.com>
2026-02-18 23:14:53 +0000

    build based on 3165bad

---
+++ v1.41.6/gallery/pgfplotsx/generated/pgfplotsx-ref017.jl
@@ -0,0 +1,17 @@
+using Plots
+pgfplotsx()
+
+Plots.reset_defaults()  #hide
+using StableRNGs  #hide
+rng = StableRNG(1234)  #hide
+nothing  #hide
+
+plot(Plots.fakedata(rng, 100, 10), layout = 4, palette = cgrad.([:grays :blues :heat :lightrainbow]), bg_inside = [:orange :pink :darkblue :black])
+current()  #hide
+
+using Random
+Random.seed!(111)
+plot!(Plots.fakedata(rng, 100, 10))
+current()  #hide
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
